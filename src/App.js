ba93bc8d15b18f572f094d2d77c5b547f542dbf1
Doug Zuniga <softdevninja@gmail.com>
2024-07-24 21:15:43 -0400

    testing deploy with custom domain

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router} from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
+      <Router basename='/'>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
@@ -18,6 +20,7 @@ function App() {
           Learn React
         </a>
       </header>
+      </Router>
     </div>
   );
 }
